f740336b44a69f1ece460a40e08f10e8aad2f107
Vico4 <victoire.stahl@gmail.com>
2024-03-29 23:06:52 +0100

    no frenglish

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -41,15 +41,11 @@ export const login = async (req: Request, res: Response) => {
   try {
     const user = await UserModel.findOne({ email: req.body.email });
     if (!user) {
-      return res
-        .status(401)
-        .json({ message: "Paire login/mot de passe incorrecte" });
+      return res.status(401).json({ message: "Incorrect login informations" });
     }
     const isValid = await bcrypt.compare(req.body.password, user.password);
     if (!isValid) {
-      return res
-        .status(401)
-        .json({ message: "Paire login/mot de passe incorrecte" });
+      return res.status(401).json({ message: "Incorrect login informations" });
     }
     res.status(200).json({
       email: user.email,
