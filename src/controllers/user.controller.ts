21aa69ad21b0aa416c9b104dd56b64c55f0d7bd2
Vico4 <victoire.stahl@gmail.com>
2024-03-29 22:11:58 +0100

    refacto as async await

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,60 +4,60 @@ import bcrypt from "bcrypt";
 
 import { Request, Response } from "express";
 
-export const signup = (req: Request, res: Response) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new UserModel({
-        email: req.body.email,
-        password: hash,
-        derbyName: req.body.name,
-        isAdmin: req.body.isAdmin,
-        earnedPoints: 0,
-      });
-      user
-        .save()
-        .then(() =>
-          res.status(201).json({
-            ...user,
-            token: jwt.sign(
-              { userId: user._id, isAdmin: user.isAdmin },
-              process.env.JWTSECRET as Secret,
-              { expiresIn: "7d" },
-            ),
-          }),
-        )
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+
+    const user = await UserModel.create({
+      email: req.body.email,
+      password: hash,
+      derbyName: req.body.name,
+      isAdmin: req.body.isAdmin,
+      earnedPoints: 0,
+    });
+
+    res.status(201).json({
+      email: user.email,
+      derbyName: user.derbyName,
+      isAdmin: user.isAdmin,
+      earnedPoints: user.earnedPoints,
+      token: jwt.sign(
+        { userId: user._id, isAdmin: user.isAdmin },
+        process.env.JWTSECRET as Secret,
+        { expiresIn: "7d" },
+      ),
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
-export const login = (req: Request, res: Response) => {
-  UserModel.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res
-          .status(401)
-          .json({ message: "Paire login/mot de passe incorrecte" });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res
-              .status(401)
-              .json({ message: "Paire login/mot de passe incorrecte" });
-          }
-          res.status(200).json({
-            ...user,
-            token: jwt.sign(
-              { userId: user._id, isAdmin: user.isAdmin },
-              process.env.JWTSECRET as Secret,
-              { expiresIn: "7d" },
-            ),
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+export const login = async (req: Request, res: Response) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Paire login/mot de passe incorrecte" });
+    }
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+    if (!isValid) {
+      return res
+        .status(401)
+        .json({ message: "Paire login/mot de passe incorrecte" });
+    }
+    res.status(200).json({
+      email: user.email,
+      derbyName: user.derbyName,
+      isAdmin: user.isAdmin,
+      earnedPoints: user.earnedPoints,
+      token: jwt.sign(
+        { userId: user._id, isAdmin: user.isAdmin },
+        process.env.JWTSECRET as Secret,
+        { expiresIn: "7d" },
+      ),
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
