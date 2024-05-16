43761091f06ef7470488b58c6d40640330fbf0af
Victoire Stahl <v.stahl@citron.io>
2024-05-16 13:39:26 +0200

    less strict password rule

--- src/validation/userValidation.schema.ts
+++ src/validation/userValidation.schema.ts
@@ -1,25 +1,13 @@
 import { z } from "zod";
 
-const containsUppercase = (ch: string) => /[A-Z]/.test(ch);
-const containsLowercase = (ch: string) => /[a-z]/.test(ch);
-const containsSpecialChar = (ch: string) =>
-  /[`!@#$%^&*()_\-+=[\]{};':"\\|,.<>/?~ ]/.test(ch);
-
 const pwRequirement =
-  "your password must be at least 8 characters long, contain uppercase, lowercase and special characters";
+  "your password must be at least 8 characters long";
 
 export const userCreationSchema = z.object({
   email: z.string().email(),
   password: z
     .string()
-    .min(8, { message: `password too short, ${pwRequirement}` })
-    .refine(
-      (pw) =>
-        containsUppercase(pw) &&
-        containsLowercase(pw) &&
-        containsSpecialChar(pw),
-      { message: `password too simple, ${pwRequirement}` },
-    ),
+    .min(8, { message: `password too short, ${pwRequirement}` }),
   derbyName: z.string().max(24, {
     message: "waouh, that's a long derby name ! Mind using a shorter one ?",
   }),
